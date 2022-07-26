de94e0cdbd168c8f904fdcb755081820a402f49e
pinetreelch <chanho.lee0215@gmail.com>
2022-07-26 16:15:05 +0900

    java start

---
+++ src/com/pinetreelch/study/ch02/Hello.java
@@ -0,0 +1,50 @@
+package com.pinetreelch.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 데이타타입 변수명 = 초기값; 
+	String bookName= "포로세스 이코노미";
+	String bookNamesub="아웃풋이 아닌 프로세스를 파는 새로운 가치 전략";
+	
+	String author = "오바라 가즈히로";
+	String publisher = "인플루엔션";
+	
+	int bookpriceOrigin = 16000;
+	int discountPrice = 4000;
+	int salePrice = 12000;
+	
+	String bookIntro = "이 책은 프로세스 이코노미";
+	String opinion = "아마존 종합 베스트 1위";
+	String authorProfile = "오바라 가즈히로";
+	String authorEducationbg = "고려대학교 국문학과";
+	String authorcareer = "문학상 수상";
+	
+	String authorIntro = "일본에 사는 저자";
+	String tableOfcontents = "1장 프로세스인가?";
+	
+	System.out.println(authorIntro);
+	System.out.println(salePrice);
+	System.out.println(1000);
+	System.out.println("문자열"+"   더하기");
+	
+	System.out.println("책이름:"+bookName);
+	System.out.println("부제:"+bookNamesub);
+	System.out.println("저자:"+author);
+	System.out.println("출판사:"+publisher);
+	System.out.println("종이책원가:"+bookpriceOrigin);
+	System.out.println("할인금액:"+discountPrice);
+	System.out.println("판매가:"+salePrice);
+	System.out.println("책 소개:"+bookIntro);
+	System.out.println(opinion);
+	System.out.println(authorProfile);
+	System.out.println(authorEducationbg);
+	System.out.println(authorcareer);
+	System.out.println(authorIntro);
+	System.out.println(tableOfcontents);
+	
+	
+
+	}
+}
