94798847e38b3dbae3a611e791b967562962ba27
pinetreelch <chanho.lee0215@gmail.com>
2022-07-29 17:17:40 +0900

    java 변수 타입

---
+++ src/com/pinetreelch/study/ch02/BooleanExample.java
@@ -0,0 +1,18 @@
+package com.pinetreelch.study.ch02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean stop = false; //true, false
+		
+		if(stop) {
+			
+			System.out.println("중지합니다");
+		} else {
+			System.out.println("시작합니다");
+		}
+	}
+
+}
---
+++ src/com/pinetreelch/study/ch02/LongExample.java
@@ -0,0 +1,20 @@
+package com.pinetreelch.study.ch02;
+
+public class LongExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long var1 = 10;
+		long var2 = 2045654644;
+		//long var2 = 2045654644;
+		
+		//long var3 = 100000000000000;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		//System.out.println(var3);
+		
+	}
+
+}
