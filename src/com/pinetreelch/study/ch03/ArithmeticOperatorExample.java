84204fc3f6c29534cc0c5d9b81e2c6f5bb0f419e
pinetreelch <chanho.lee0215@gmail.com>
2022-08-03 17:07:00 +0900

    함수 생성

--- src/com/pinetreelch/study/ch03/ArithmeticOperatorExample.java
+++ src/com/pinetreelch/study/ch03/ArithmeticOperatorExample.java
@@ -33,6 +33,45 @@ public class ArithmeticOperatorExample {
 		
 		double result6 = (double) y1 / y2;
 		System.out.println("result6: "+result6);
+		
+		
+		double a1 = ((1+2/3)-5/8)*(9+4/5);
+		
+		System.out.println("1번문제: "+ a1);
+		
+		double a2 = 5-(3.25-(1+5/6))*(12/17);
+		System.out.println("2번 문제: "+ a2);
+		
+		int b1 = 8+7+5;
+		int b2 = 1+9+5;
+		int b3 = 5+6+6;
+		int b4 = 4+7+1;
+		int b5 = 7+5+5;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(b3);
+		System.out.println(b4);
+		System.out.println(b5);
+		
+		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
+		 
+		divide("홍길동",60,70,85,90,45);
+		divide("박말순",50,75,70,60,70);
+		divide("고장난",55,60,64,58,90);
+		divide("도룡뇽",80,70,63,88,78);
 	}	
+	
+	public static void sum(int a, int b, int c) {
+		System.out.println(a+"+"+b+"+"+c+"="+(a+b+c));
+	}
+	
+	public static void divide(String name, int english, int math, int physics, int music, int athletic) {
+		System.out.println(name+"(평균): "+ (double)(english+math+physics+music+athletic)/5);
+	}
 
 }
