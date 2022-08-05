f9b238b68eb2b60b9b9eb116422c8dbc88568759
pinetreelch <chanho.lee0215@gmail.com>
2022-08-05 17:28:34 +0900

    +- -+ *= 연산자

---
+++ src/com/pinetreelch/study/ch03/AssignmentOperationExample.java
@@ -0,0 +1,31 @@
+package com.pinetreelch.study.ch03;
+
+public class AssignmentOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// +=
+		
+		int result = 0;
+		
+		result += 10;
+		//result = result + 10;
+		System.out.println("result: "+ result);
+		
+		//-=
+		
+		int result2 = 2;
+		
+		result2 -= 2;
+		System.out.println("result2: "+ result2);
+		
+		
+		//*=
+		int result3 = 3;
+		result3 *= 5;
+		System.out.println("result3: "+ result3);
+		
+	}
+
+}
