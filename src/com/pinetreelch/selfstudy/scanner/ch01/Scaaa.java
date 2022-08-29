32a3e915571002cb45cac4351cffee5dcc5a965f
pinetreelch <chanho.lee0215@gmail.com>
2022-08-30 00:45:40 +0900

    문자 갯수 찾기

--- src/com/pinetreelch/selfstudy/scanner/ch01/Scaaa.java
+++ src/com/pinetreelch/selfstudy/scanner/ch01/Scaaa.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 public class Scaaa {
 
+	public static int solution(String str, char ch) {
+		
+		str.toUpperCase();
+		Character.toUpperCase(ch);
+		int answer = 0;
+		for (char x : str.toCharArray()) {
+			if(x == ch) answer ++;
+		}
+		return answer;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String str = sc.nextLine();
-		System.out.println(str);
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		char ch = kb.next().charAt(0);
+		
+		System.out.println(solution(str, ch));
 	}
 
 }
