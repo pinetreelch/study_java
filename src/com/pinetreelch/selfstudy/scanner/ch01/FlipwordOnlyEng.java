2969b94f0c56a5cc8548d96255d3a9b1a64619e3
pinetreelch <chanho.lee0215@gmail.com>
2022-09-07 17:43:19 +0900

    왜 안돼

--- src/com/pinetreelch/selfstudy/scanner/ch01/FlipwordOnlyEng.java
+++ src/com/pinetreelch/selfstudy/scanner/ch01/FlipwordOnlyEng.java
@@ -5,47 +5,71 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FlipwordOnlyEng {
-	
-	public static ArrayList<String> solution(String str){
-		
+
+	public static ArrayList<String> solution(String str) {
+
 		ArrayList<String> answer = new ArrayList<>();
-		char[] ch = str.toCharArray();			// if @@abcd
-		
+		char[] ch = str.toCharArray(); // if @@abcd
+
 		int lt = 0;
 		int rt = ch.length - 1;
+
 		
-		/*
-		 * while(lt < rt) { if((ch[lt] >= 97 && ch[lt] <= 122) || (ch[lt] <= 90 &&
-		 * ch[lt] >= 65)) { char tmp = ch[lt];
-		 * 
-		 * if((ch[rt] >= 97 && ch[rt] <= 122) || (ch[rt] <= 90 && ch[rt] >= 65)) {
-		 * ch[lt] = ch[rt]; ch[rt] = tmp; } else { rt--; } } else { lt++; }
-		 */
-		while(lt<rt) {
-			
-			char tmp = ch[lt];
-			ch[lt] = ch[rt];
-			ch[rt] = tmp;
-			lt ++;
-			rt --;
+//		 while(lt < rt) { if((ch[lt] >= 97 && ch[lt] <= 122) || (ch[lt] <= 90 &&ch[lt] >= 65)) { 
+//			 char tmp = ch[lt];
+//			 
+//			 if((ch[rt] >= 97 && ch[rt] <= 122) || (ch[rt] <= 90 && ch[rt] >= 65)) {
+//				 	ch[lt] = ch[rt];
+//				 	ch[rt] = tmp; 
+//			} else { rt--; } } else { lt++; }
+//		 }
+		  
+		while (lt < rt) {
+			if( (ch[lt] >= 97 && ch[lt] <= 122) || (ch[lt] <= 90 && ch[lt] >= 65) ) {
+				char tmp = ch[lt];
+				
+				if( (ch[rt] >= 97 && ch[rt] <= 122) || (ch[rt] <= 90 && ch[rt] >= 65) ) {
+					ch[lt] = ch[rt];
+					ch[rt] = tmp;
+				} else {
+					rt --;
+				}
+			} else {
+				lt ++;
+			}
 		}
-		String chString = String.valueOf(ch);
-		answer.add(chString);
 		
+		String chString = String.valueOf(ch);
+
+		answer.add(chString);
+
 		return answer;
+		
+
+//		
+//		while(lt<rt) {
+//			
+//			char tmp = ch[lt];
+//			ch[lt] = ch[rt];
+//			ch[rt] = tmp;
+//			lt ++;
+//			rt --;
+//		}
+//		
+
+
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
-		
+
 		String str = kb.next();
-	
-		for(String x : solution(str)) {
-			System.out.println(solution(x));
+
+		for (String x : solution(str)) {
+			System.out.println(x);
 		}
-		
-		
+
 	}
 
 }
