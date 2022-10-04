17541593aebbb9324b2cb444ecb2947e9cbf4038
pinetreelch <chanho.lee0215@gmail.com>
2022-10-04 17:31:41 +0900

    콜랙션, 프로퍼티, 펙터,

---
+++ src/com/pinetreelch/study/ch12/first/BeepPrintExample.java
@@ -0,0 +1,22 @@
+package com.pinetreelch.study.ch12.first;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+
+	public static void main(String[] args) {
+		
+
+		Toolkit toolkit = Toolkit.getDefaultToolkit();	
+		for(int i=0; i<5; i++) {		
+			toolkit.beep();
+			try { Thread.sleep(500); } catch(Exception e) {}
+		}
+
+		for(int i=0; i<5; i++) {
+			System.out.println("12");
+			try { Thread.sleep(500); } catch(Exception e) {}
+		}
+	}
+
+}
