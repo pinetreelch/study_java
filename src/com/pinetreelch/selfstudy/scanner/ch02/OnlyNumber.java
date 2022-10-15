d0a6ac0f2ee5b31d13b5a4ac5db51bfaa2069482
pinetreelch <chanho.lee0215@gmail.com>
2022-10-15 20:38:05 +0900

    향상된 유효한 펠린드롬 nextline 사

---
+++ src/com/pinetreelch/selfstudy/scanner/ch02/OnlyNumber.java
@@ -0,0 +1,32 @@
+package com.pinetreelch.selfstudy.scanner.ch02;
+
+import java.util.Scanner;
+
+public class OnlyNumber {
+	
+	public static Integer solution(String str) {
+		
+		int num = 0;
+		
+		char[] ch = str.toCharArray();
+		
+		
+		
+		return num;
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner kb = new Scanner(System.in);
+		
+		String str = kb.next();
+		
+		System.out.println(solution(str));
+
+	}
+
+}
--- src/com/pinetreelch/selfstudy/scanner/ch02/PalindromeAdvance.java
+++ src/com/pinetreelch/selfstudy/scanner/ch02/PalindromeAdvance.java
@@ -4,12 +4,44 @@ import java.util.Scanner;
 
 public class PalindromeAdvance {
 	
-	public static Boolean solution(String str) {
+	public static String solution(String str) {
 		
-		Boolean answer = true; 
+		String answer = "NO"; 
+		String word = "";
+		String word2 = "";
+		
+		String receive = str.toUpperCase();
+		
+		StringBuffer sb = new StringBuffer(receive);
+		
+		String rreceive = sb.reverse().toString();
+
+		char[] ch = receive.toCharArray();
+		char[] ch2 = rreceive.toCharArray();
+		
+		for(char c: ch) {
+			if(Character.isLetter(c)) {
+				word = word + c;
+			} else {
+				//bypass
+			}
+			
+		}
+		
+		for(char c : ch2) {
+			if(Character.isLetter(c)) {
+				word2 = word2 + c;
+			} else {
+				//bypass
+			}
+		}
 		
 		
 		
+		
+		if(word.equals(word2)) {
+			answer = "YES" ;
+		} 
 		return answer;
 		
 	}
@@ -19,9 +51,8 @@ public class PalindromeAdvance {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
 		
-		String str = kb.next();
+		String str = kb.nextLine();
 		
 		System.out.println(solution(str));
 	}
-
 }
